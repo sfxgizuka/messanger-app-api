c164dd0a0e13bdbed51b5bdc9d5f6d08dfceded2
Gregory Izuka <gizuka@seamfix.com>
2023-06-28 22:08:38 +0100

    added server

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 import express from 'express';
 
 const app = express();
+const port = process.env.PORT || 5000
+
+app.get('/',(req, res)=>{
+    res.send('This is from backend')
+})
+
+app.listen(port, ()=>{
+    console.log(`app is running on ${port}`);
+})
