90c9d1312ebd2e777fb88d4df00958ba0f2ffd0e
Gregory Izuka <gizuka@seamfix.com>
2023-12-31 20:16:36 +0100

    message send image

--- src/controller/messangerController.ts
+++ src/controller/messangerController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import User from '../model/authModel';
 import messageModel from '../model/messageModel';
+import formidable from 'formidable';
+import fs from 'fs';
+import path from 'path'
 
 export const getFriends = async (req:any, res:Response) => {
     const myId = req?.myId;
@@ -75,4 +78,58 @@ export const messageGet = async(req:any, res:Response) => {
 
      }
 
+}
+
+export const ImageMessageSend = (req:any, res:Response) => {
+     const senderId = req.myId;
+     const form = formidable();
+
+     form.parse(req, (err, fields, files:any) => {
+          const {
+              senderName,
+              receiverId,
+              imageName 
+          } = fields;
+
+          // const newPath = __dirname + `../../../frontend/public/image/${imageName}`
+          // const newPath = __dirname + `./${imageName}`
+          const newPath = path.join(__dirname, '../../public/images', files.image.originalFilename);
+          files.image.originalFilename = imageName;
+
+          try{
+               fs.copyFile(files.image.filepath, newPath, async (err)=>{
+                    if(err){
+                         res.status(500).json({
+                              error : {
+                                   errorMessage: 'Image upload fail'
+                              }
+                         })
+                    } else{
+                         const insertMessage = await messageModel.create({
+                              senderId : senderId,
+                              senderName : senderName,
+                              receiverId : receiverId,
+                              message : {
+                                   text: '',
+                                   image : files.image.originalFilename
+                              }
+                         })
+                         res.status(201).json({
+                              success : true,
+                              message: insertMessage
+                         })
+
+                    }
+               } )
+
+          }catch{
+               res.status(500).json({
+                    error : {
+                         errorMessage: 'Internal Sever Error'
+                    }
+               })
+          }
+
+
+     })
 }
--- src/routes/messengerRoute.ts
+++ src/routes/messengerRoute.ts
@@ -1,5 +1,5 @@
 import  {Router} from  'express'
-import {getFriends, messageGet, messageUploadDB} from '../controller/messangerController';
+import {ImageMessageSend, getFriends, messageGet, messageUploadDB} from '../controller/messangerController';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -8,6 +8,7 @@ const router = Router();
 router.get('/get-friends',authMiddleware,getFriends);
 router.post('/send-message',authMiddleware, messageUploadDB);
 router.get('/get-message/:id',authMiddleware, messageGet);
+router.post('/image-message-send',authMiddleware, ImageMessageSend);
 
 
 export default router;
